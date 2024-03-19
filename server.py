07d7153fd512bfe7d4e844afb5fe55061857cd5c
Revanth-2717 <152462274+Revanth-2717@users.noreply.github.com>
2024-03-19 21:00:35 +0530

    Success

---
+++ server.py
@@ -0,0 +1,55 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<html>
+    <head>
+         <title>simplewebserver</title>
+    </head>
+    <body>
+        <table align="center" border="2" cellspacing="12" cellpadding="12" height="25" width="50">
+        <h1 align="center" >top 5 software companies with revenue</h1>
+            <tr>
+            
+                <th>RANK</th>
+                <th>COMPANY</th>
+                <th>REVENUE</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Microsoft</td>
+                <td>$86.8</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>oracle</td>
+                <td>$37.1</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>SAP</td>
+                <td>$20.9</td>
+            </tr>
+             <tr>
+                <td>4</td>
+                <td>symantec</td>
+                <td>$6.8</td>
+            </tr>
+             <tr>
+                <td>5</td>
+                <td>VMware</td>
+                <td>$5.2</td>
+            </tr>
+        </table>
+    </body>
+<html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',80)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
